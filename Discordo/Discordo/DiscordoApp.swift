889a0350fbc50f3725bacaef05db21ef4d504395
AlexSMFerreira <alexandre.smferreira@gmail.com>
2024-06-27 00:46:14 +0100

    Getting Started

---
+++ Discordo/Discordo/DiscordoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordoApp.swift
+//  Discordo
+//
+//  Created by Paulo Ferreira on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscordoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
